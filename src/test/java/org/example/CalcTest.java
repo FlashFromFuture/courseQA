7b5e0e1603c828c8339bb538138a6a2ac9db39c1
FlashFromFuture <techee@mail.ru>
2024-07-22 13:30:19 +0400

    add  two test for "add" nad "sub"

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -17,5 +17,8 @@ class CalcTest {
 
     @Test
     void sub() {
+        Calc calc = new Calc();
+        int result = calc.sub(10, 6);
+        Assertions.assertEquals(4, result, "Неправильный ответ" );
     }
 }
